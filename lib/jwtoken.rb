752624a428850e6a61727e761ed532558f7fa0a8
Alex Wheeler <afwheeler92@gmail.com>
2020-03-17 17:21:56 -0600

    commit number 1 \m/

---
+++ lib/jwtoken.rb
@@ -0,0 +1,6 @@
+require "jwtoken/version"
+
+module Jwtoken
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/jwtoken_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Jwtoken do
+  it "has a version number" do
+    expect(Jwtoken::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
